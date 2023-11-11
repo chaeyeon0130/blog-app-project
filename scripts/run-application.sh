d591d0334999793a78c41e73ddea470f5f5ebeac
chaeyeon0130 <k84292330@gmail.com>
2023-11-11 11:55:30 +0900

    chore: 실행 파일 확장자 명시

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -1,3 +1,6 @@
 sudo pkill -6 java
 source /home/ubuntu/.env
-nohup java -jar /home/ubuntu/yourssu_project/build/libs/*.jar 1>>/home/ubuntu/log/spring-log.log 2>>/home/ubuntu/log/spring-error.log &
+REPOSITORY=/home/ubuntu/yourssu_project
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+nohup java -jar $JAR_PATH 1>>/home/ubuntu/log/spring-log.log 2>>/home/ubuntu/log/spring-error.log &
